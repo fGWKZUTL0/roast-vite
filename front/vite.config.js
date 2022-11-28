6a0c2f930dc676b71f3e8ef540297fcbf701965c
fGWKZUTL0 <onatik0198@gmail.com>
2022-11-28 17:16:45 +0900

    HMR有効化

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+  ],
   server: {
-    host: true
+    host: true,
+    watch: {
+      usePolling: true
+    }
   }
 })
